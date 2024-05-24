3c63849badba5a174529aa853cde071ba5a33ca2
MiuraHideyoshi <k846996@kansai-u.ac.jp>
2024-05-24 09:16:04 +0000

    rec and send

--- receive_miu.py
+++ receive_miu.py
@@ -7,59 +7,34 @@ from scapy.all import IP, TCP, Ether, get_if_hwaddr, get_if_list, sendp
 import time
 import re
 
-def my_parse(data):
-    pkt = bytes(packet)
-    TCMP_PROTO = 1
-    TCP_PROTO = 6
-    UDP_PROTO = 17
-    COMP_PROTO = 146
-
-    ETHERNET_HEADER_LENGTH = 14
-    IP_HEADER_LENGTH = 20
-    ICMP_HEADER_LENGTH = 8
-    UDP_HEADER_LENGTH = 8
-    TCP_HEADER_LENGTH = 20
-    COMP_HEADER_LENGTH = 14
-    
-    raw = bytes(packet) # to get payload
-    v1 = ETHERNET_HEADER_LENGTH + IP_HEADER_LENGTH
-    #print(raw)
-    print(packet[v1+1:COMP_HEADER_LENGTH])
-
-def expand(x):
-    yield x
-    while x.payload:
-        x = x.payload
-        yield x
 global count
 count = 0
 def packet_callback(packet):
     pkt = bytes(packet)
     global count
-    data = str(packet.show)
-    TCMP_PROTO = 1
-    TCP_PROTO = 6
-    UDP_PROTO = 17
-    COMP_PROTO = 146
-
-    ETHERNET_HEADER_LENGTH = 14
-    IP_HEADER_LENGTH = 20
-    ICMP_HEADER_LENGTH = 8
-    UDP_HEADER_LENGTH = 8
-    TCP_HEADER_LENGTH = 20
-    COMP_HEADER_LENGTH = 14
+    IP_PROTO_ICMP = 1
+    IP_PROTO_TCP  = 6
+    IP_PROTO_UDP  = 17
+    IP_PROTO_COMP = 146 ## user defined header
+    
+    HEADER_LENGTH_ETHERNET = 14
+    HEADER_LENGTH_IP       = 20
+    HEADER_LENGTH_ICMP     = 8
+    HEADER_LENGTH_UDP      = 8
+    HEADER_LENGTH_TCP      = 20
+    HEADER_LENGTH_COMP     = 14 ## user defined header
     
     print(" ----------------------------------- packet-in -----------------------------------")
     indent = " "
     now_indent = indent
     ## Ethernet header report ## ------------------------
     pre_pos = 0
-    end_pos = ETHERNET_HEADER_LENGTH
+    end_pos = HEADER_LENGTH_ETHERNET
     eth_report = Ether(pkt[pre_pos:end_pos])
     eth_report.show2()    
     ## IPv4 header report ## ------------------------
     pre_pos = end_pos
-    end_pos += IP_HEADER_LENGTH
+    end_pos += HEADER_LENGTH_IP
     ip_report = IP(pkt[pre_pos:end_pos])
     ip_report.show2(lvl=now_indent)
     
@@ -71,22 +46,22 @@ def packet_callback(packet):
             print("### [COMP HEADER {}] ###".format(comp_header_count))
             comp_header_count += 1
             pre_pos = end_pos
-            end_pos += COMP_HEADER_LENGTH
+            end_pos += HEADER_LENGTH_COMP
             comp_report = COMP(pkt[pre_pos:end_pos])
             for cnt in range(comp_header_count):
                 now_indent += indent
             comp_report.show2(lvl=now_indent)
             compType = comp_report.compType
         ## TCP header report ## ------------------------
-        if compType == TCP_PROTO:
+        if compType == IP_PROTO_TCP:
             pre_pos = end_pos
-            end_pos += TCP_HEADER_LENGTH
+            end_pos += HEADER_LENGTH_TCP
             tcp_report = TCP(pkt[pre_pos:end_pos])
             now_indent += indent
             tcp_report.show2(lvl=now_indent)
-        elif compType == UDP_PROTO:
+        elif compType == IP_PROTO_UDP:
             pre_pos = end_pos
-            end_pos += UDP_HEADER_LENGTH
+            end_pos += HEADER_LENGTH_UDP
             udp_report = UDP(pkt[pre_pos:end_pos])
             now_indent += indent
             udp_report.show2(lvl=now_indent)
